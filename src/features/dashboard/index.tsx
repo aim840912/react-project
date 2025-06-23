273e8d9385e867dae28a223e917a5a793649bad1
Tien <tien@mochibits.com>
2025-06-23 21:57:50 +0800

    測試使用react-grid-layout

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -2,14 +2,40 @@ import { Row, Col, Card, Progress, Statistic, Timeline, Tag } from "antd";
 import { RadarChartOutlined, SnippetsOutlined, DollarOutlined, LaptopOutlined } from "@ant-design/icons";
 import ReactECharts from "echarts-for-react";
 import "./index.scss";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import type { Layout } from "react-grid-layout";
+
 import { EnergyChartSeries, EnergyItem } from '../energy/types';
 import type { EChartsOption } from 'echarts';
 import { initialOption, option, nightingaleOption } from './dashboard.charts';
 import { useLoadEnergyDataQuery } from "./api/dashboardApi";
 
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
 function Dashboard() {
     const { data: energyData, isLoading } = useLoadEnergyDataQuery();
+
+    const initialLayouts: { lg: Layout[] } = {
+        lg: [
+            { i: "a", x: 0, y: 0, w: 3, h: 1 },
+            { i: "b", x: 3, y: 0, w: 3, h: 1 },
+            { i: "c", x: 6, y: 0, w: 3, h: 1 },
+            { i: "d", x: 9, y: 0, w: 3, h: 1 },
+            { i: "e", x: 0, y: 2, w: 6, h: 4 },
+            { i: "f", x: 6, y: 2, w: 6, h: 4 },
+            { i: "g", x: 0, y: 6, w: 6, h: 4 },
+            { i: "h", x: 6, y: 6, w: 3, h: 4 },
+            { i: "i", x: 9, y: 6, w: 3, h: 4 },
+        ],
+    };
+
+    const [layouts, setLayouts] = useState<{ [key: string]: Layout[] }>(initialLayouts);
+
+    const onLayoutChange = (layout: Layout[], allLayouts: { [key: string]: Layout[] }) => {
+        setLayouts(allLayouts);
+        // localStorage.setItem('dashboard-layouts', JSON.stringify(allLayouts));
+    };
 
     const chartOption: EChartsOption = useMemo(() => {
         if (!energyData) {
@@ -33,9 +59,16 @@ function Dashboard() {
     }, [energyData]);
     return (
         <div className="dashboard">
-            <Row gutter={16}>
-                <Col span={6}>
-                    <Card className="clearfix">
+            <ResponsiveGridLayout
+                className="layout"
+                layouts={layouts}
+                breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                rowHeight={100}
+                onLayoutChange={onLayoutChange}
+            >
+                <div key="a">
+                    <Card className="clearfix  h-full" >
                         <div className="fl area">
                             <h2>13479</h2>
                             <p>園區總面積(平方米)</p>
@@ -44,9 +77,9 @@ function Dashboard() {
                             <RadarChartOutlined className="icon" />
                         </div>
                     </Card>
-                </Col>
-                <Col span={6}>
-                    <Card className="clearfix">
+                </div>
+                <div key="b">
+                    <Card className="clearfix h-full">
                         <div className="fl area">
                             <h2>8635</h2>
                             <p>總租賃面積(平方米)</p>
@@ -55,9 +88,9 @@ function Dashboard() {
                             <SnippetsOutlined className="icon" style={{ color: "#81c452" }} />
                         </div>
                     </Card>
-                </Col>
-                <Col span={6}>
-                    <Card className="clearfix">
+                </div>
+                <div key="c">
+                    <Card className="clearfix h-full">
                         <div className="fl area">
                             <h2>38764</h2>
                             <p>園區總產值(萬元)</p>
@@ -66,9 +99,9 @@ function Dashboard() {
                             <DollarOutlined className="icon" style={{ color: "#62c9cb" }} />
                         </div>
                     </Card>
-                </Col>
-                <Col span={6}>
-                    <Card className="clearfix">
+                </div>
+                <div key="d">
+                    <Card className="clearfix h-full">
                         <div className="fl area">
                             <h2>2874</h2>
                             <p>入駐企業總數(家)</p>
@@ -77,35 +110,36 @@ function Dashboard() {
                             <LaptopOutlined className="icon" style={{ color: "#e49362" }} />
                         </div>
                     </Card>
-                </Col>
-            </Row>
-            <Row gutter={16} className="mt">
-                <Col span={12}>
+                </div>
+
+                <div key="e" className="mt">
                     <Card title="能源消耗情況">
                         <ReactECharts option={chartOption} showLoading={isLoading} />
                     </Card>
-                </Col>
-                <Col span={12}>
+                </div>
+                <div key="f" className="mt">
                     <Card title="企業資質情況">
                         <ReactECharts option={option} />
                     </Card>
-                </Col>
-            </Row>
-            <Row gutter={16} className="mt">
-                <Col span={12}>
+                </div>
+
+
+                <div key="g">
                     <Card title="租賃情況">
                         <ReactECharts option={nightingaleOption} />
                     </Card>
-                </Col>
-                <Col span={6}>
+                </div>
+
+                <div key="h">
                     <Card title="充電樁空閒統計">
                         <div className="wrap">
                             <Progress type="circle" percent={75} />
                             <Statistic title="總充電樁數" value={75} suffix="/ 100" className="mt" />
                         </div>
                     </Card>
-                </Col>
-                <Col span={6}>
+                </div>
+
+                <div key="i">
                     <Card title="實時車輛信息" style={{ height: "406px" }}>
                         <Timeline items={[
                             { key: 'timeline-item-1', children: <><Tag color="green">進場</Tag>08:24車輛 京A66666</> },
@@ -116,8 +150,9 @@ function Dashboard() {
                             { key: 'timeline-item-6', children: <><Tag color="green">進場</Tag>14:46 車輛 京A23456</> },
                         ]} />
                     </Card>
-                </Col>
-            </Row>
+                </div>
+
+            </ResponsiveGridLayout>
         </div>
     );
 }
